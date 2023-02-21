e50d9978ebc527e6adcd24b81fea64d3d1bfe8d6
Criptc <96071765+Criptc@users.noreply.github.com>
2023-02-21 15:55:01 -0600

    Update e621.js

--- src/commands/e621.js
+++ src/commands/e621.js
@@ -4,36 +4,38 @@ const limit_max = 320 // e621 max, should prob be lower
 
 module.exports = {
     data: new SlashCommandBuilder()
-	.setName('e621')
-	.setDescription('Get posts from e621 or e926.')
-	.addUserOption(option => option.setName('tags').setDescription('Tags to search with'))
-        .addUserOption(option => option.setName('limit').setDescription('Amount of posts to return'))
+		.setName('e621')
+		.setDescription('Get posts from e621 or e926.')
+		.addUserOption(option => option.setName('tags').setDescription('Tags to search with'))
+		.addUserOption(option => option.setName('limit').setDescription('Amount of posts to return'))
         .addUserOption(option => option.setName('NSFW').setDescription('whether to have NSFW ')),
 	async execute(interaction) {
 		const tags = interaction.options.getString('tags')
 		let limit = interaction.options.getInt('limit')
 		const nsfw = interaction.options.getBool('NSFW')
+		let eurl
 
 		if (nsfw){
-		    const url = "https://e621.net/"
+		    eurl = "https://e621.net/"
 		} else {
-		    const url = "https://e926.net/"
+		    eurl = "https://e926.net/"
 		}
 
 		if (limit === null){
 		    limit = 5
+		} else if (limit > limit_max){
+			limit = limit_max
 		}
 
-		const response = await fetch(`${url}/post.json?tags=${encodeURIComponent(tags)}&limit=${limit}`, {
+		const response = await fetch(`${eurl}/post.json?tags=${encodeURIComponent(tags)}&limit=${limit}`, {
 		    method: "GET",
 		    headers: {
 			"User-Agent": "Feral/1.0.0"
 		    }
 		})
 
-		for (const [key, value] of Object.entries(response.json()["posts"])) {
-		    interaction.reply(`link: \`${url}/posts/${value.id}\`\nrating: \`${value.rating}\`\nscore: \`${value.score.total}\`\n${value.file.url}`)
+		for (const [, value] of Object.entries(await await response.json()["posts"])) {
+		    interaction.reply(`link: \`${eurl}/posts/${value.id}\`\nrating: \`${value.rating}\`\nscore: \`${value.score.total}\`\n${value.file.url}`)
 		}
-		return
 	},
 }
