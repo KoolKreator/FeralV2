7523b04f57c5ed7da6c3b3c89076a5c3ad2304a9
Criptc <96071765+Criptc@users.noreply.github.com>
2023-02-21 16:31:55 -0600

    fixed badly named getoptions

--- src/commands/e621.js
+++ src/commands/e621.js
@@ -3,43 +3,39 @@ const {SlashCommandBuilder} = require('discord.js');
 const limit_max = 320 // e621 max, should prob be lower
 
 module.exports = {
-    data: new SlashCommandBuilder()
-	.setName('e621')
-	.setDescription('Get posts from e621 or e926.')
-	.addUserOption(option => option.setName('tags').setDescription('Tags to search with'))
-	.addUserOption(option => option.setName('limit').setDescription('Amount of posts to return'))
-        .addUserOption(option => option.setName('nsfw').setDescription('whether to have NSFW ')),
+	data: new SlashCommandBuilder()
 		.setName('e621')
 		.setDescription('Get posts from e621 or e926.')
-		.addStringOption(option => option.setName('tags').setDescription('Tags to search with').setRequired(true))
-        .addNumberOption(option => option.setName('limit').setDescription('Amount of posts to return'))
-        .addBooleanOption(option => option.setName('adult').setDescription('Show adult rated post')),
+		.addUserOption(option => option.setName('tags').setDescription('Tags to search with'))
+		.addUserOption(option => option.setName('limit').setDescription('Amount of posts to return'))
+		.addUserOption(option => option.setName('nsfw').setDescription('whether to have NSFW ')),
 	async execute(interaction) {
 		const tags = interaction.options.getString('tags')
-		let limit = interaction.options.getInt('limit')
-		const nsfw = interaction.options.getBool('NSFW')
+		let limit = interaction.options.getNumber('limit')
+		const nsfw = interaction.options.getBoolean('nsfw')
+		let eurl
 
 		if (nsfw){
-		    const url = "https://e621.net/"
+			eurl = "https://e621.net/"
 		} else {
-		    const url = "https://e926.net/"
+			eurl = "https://e926.net/"
 		}
 
 		if (limit === null){
-		    limit = 5
+			limit = 5
 		} else if (limit > limit_max){
 			limit = limit_max
 		}
 
 		const response = await fetch(`${eurl}/post.json?tags=${encodeURIComponent(tags)}&limit=${limit}`, {
-		    method: "GET",
-		    headers: {
-			"User-Agent": "FeralBot/1.0 (by Kr8tiveKanine on e621)"
-		    }
+			method: "GET",
+			headers: {
+				"User-Agent": "Feral/1.0.0"
+			}
 		})
 
-		for (const [key, value] of Object.entries(response.json()["posts"])) {
-		    interaction.reply(`link: \`${url}/posts/${value.id}\`\nrating: \`${value.rating}\`\nscore: \`${value.score.total}\`\n${value.file.url}`)
+		for (const [, value] of Object.entries(await await response.json()["posts"])) {
+			interaction.reply(`link: \`${eurl}/posts/${value.id}\`\nrating: \`${value.rating}\`\nscore: \`${value.score.total}\`\n${value.file.url}`)
 		}
 	},
 }
