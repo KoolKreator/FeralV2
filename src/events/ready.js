1760c7207ad71b275988f518363877832ebad40d
Koolkreator <creativespacedev@outlook.com>
2023-02-21 14:58:56 -0500

    modified:src/events/ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,6 +2,15 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
+		setInterval(() => {
+			const statuses = [
+					`Watching For Commands`,
+					`Chasing My Tail`,
+					"Eating Your Shoe"
+				]
+			const status = statuses[Math.floor(Math.random() * statuses.length)]
+			client.user.setActivity(status, { type: "ONLINE" })
+		}, 30000)
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
