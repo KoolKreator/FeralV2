54d0ba3d8aa2df63f1e26defe73385ac217f1efb
Koolkreator <creativespacedev@outlook.com>
2023-02-21 17:05:55 -0500

    update

--- src/commands/e621.js
+++ src/commands/e621.js
@@ -4,20 +4,22 @@ const limit_max = 320 // e621 max, should prob be lower
 
 module.exports = {
     data: new SlashCommandBuilder()
-	.setName('e621')
-	.setDescription('Get posts from e621 or e926.')
-	.addUserOption(option => option.setName('tags').setDescription('Tags to search with'))
-        .addUserOption(option => option.setName('limit').setDescription('Amount of posts to return'))
-        .addUserOption(option => option.setName('NSFW').setDescription('whether to have NSFW ')),
+		.setName('e621')
+		.setDescription('Get posts from e621 or e926.')
+		.addStringOption(option => option.setName('tags').setDescription('Tags to search with').setRequired(true))
+        .addNumberOption(option => option.setName('limit').setDescription('Amount of posts to return'))
+        .addBooleanOption(option => option.setName('adult').setDescription('Show adult rated post')),
 	async execute(interaction) {
 		const tags = interaction.options.getString('tags')
-		let limit = interaction.options.getInt('limit')
-		const nsfw = interaction.options.getBool('NSFW')
+		let limit = interaction.options.getNumber('limit')
+		const rating = interaction.options.getBoolean('adult')
 
-		if (nsfw){
-		    const url = "https://e621.net/"
+		let url = ""
+
+		if (rating === true){
+			url = "https://e621.net/"
 		} else {
-		    const url = "https://e926.net/"
+			url = "https://e926.net/"
 		}
 
 		if (limit === null){
@@ -27,12 +29,12 @@ module.exports = {
 		const response = await fetch(`${url}/post.json?tags=${encodeURIComponent(tags)}&limit=${limit}`, {
 		    method: "GET",
 		    headers: {
-			"User-Agent": "Feral/1.0.0"
+			"User-Agent": "FeralBot/1.0 (by Kr8tiveKanine on e621)"
 		    }
 		})
 
-		for (const [key, value] of Object.entries(response.json()["posts"])) {
-		    interaction.reply(`link: \`${url}/posts/${value.id}\`\nrating: \`${value.rating}\`\nscore: \`${value.score.total}\`\n${value.file.url}`)
+		for (const [key, value] of Object.entries(response.json()["posts"])) {	
+		    interaction.reply(`E621/E926 Search Results..: \`${url}/posts/${value.id}\`\nrating: \`${value.rating}\`\nscore: \`${value.score.total}\`\n${value.file.url}`)
 		}
 		return
 	},
--- src/commands/ping.js
+++ src/commands/ping.js
@@ -6,7 +6,7 @@ module.exports = {
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
 		const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
-		await interaction.editReply(`:ping_pong: Pong!\n:stopwatch: Uptime: ${Math.round(interaction.client.uptime / 60000)} minutes\n:sparkling_heart: Websocket heartbeat: ${interaction.client.ws.ping}ms.\n:round_pushpin: Rountrip Latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
+		await interaction.editReply(`:ping_pong: Pong!\n:stopwatch: Uptime: ${Math.round(interaction.client.uptime / 60000)} minutes\n:sparkling_heart: Websocket heartbeat: ${interaction.client.ws.ping}ms.\n:round_pushpin: Roundtrip Latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
 	},
 		// async execute(interaction) {
 	// 	await interaction.reply('Pong!');
