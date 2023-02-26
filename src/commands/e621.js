539470b3046f652e2aa67545c279c99a9ed4077f
Koolkreator <creativespacedev@outlook.com>
2023-02-25 21:54:05 -0500

    added randomizer

--- src/commands/e621.js
+++ src/commands/e621.js
@@ -7,13 +7,17 @@ module.exports = {
 	.setName('e621')
 	.setDescription('Get posts from e621 or e926.')
 	.addStringOption(option => option.setName('tags').setDescription('Tags to search with').setRequired(true))
-    .addNumberOption(option => option.setName('limit').setDescription('Amount of posts to return'))
+    // .addNumberOption(option => option.setName('limit').setDescription('Amount of posts to return'))
     .addBooleanOption(option => option.setName('adult').setDescription('Show adult rated post')),
 	// .addBooleanOption(option => option.setName('randomize').setDescription('randomize the search result')),
 	async execute(interaction) {
 		const tags = interaction.options.getString('tags')
-		let limit = interaction.options.getNumber('limit')
+		// let limit = interaction.options.getNumber('limit')
 		const nsfw = interaction.options.getBoolean('adult')
+
+		function getRandomResponse(max){
+			return Math.floor(Math.random() * (max - 0) + 0);
+		}
 
 		if (nsfw){
 			eurl = "https://e621.net/posts"
@@ -23,13 +27,13 @@ module.exports = {
 			rate = 's'
 		}
 
-		if (limit === null){
-			limit = 5
-		} else if (limit > limit_max){
-			limit = limit_max
-		}
+		// if (limit === null){
+		// 	limit = 5
+		// } else if (limit > limit_max){
+		// 	limit = limit_max
+		// }
 
-        const url = `${eurl}.json?tags=${encodeURIComponent(tags)}&rating=${rate}&limit=${limit}`;
+        const url = `${eurl}.json?tags=${encodeURIComponent(tags)}&rating=${rate}`;
 
 		let response = await fetch(url, {
 					method: "GET",
@@ -39,11 +43,14 @@ module.exports = {
 				})
 
 		let result = await response.json();
+		let postnum = getRandomResponse(result.posts.length)
 
-		if (result['posts'].length > -1){
-			for (const [key, value] of Object.entries(result['posts'])) {
-				await interaction.reply(`link: \`${url}/posts/${value.id}\`\nrating: \`${value.rating}\`\nscore: \`${value.score.total}\`\n${value.file.url}`)
-			}
+		if (result.posts.length > -1){
+			let post = result.posts[postnum]
+			await interaction.reply(`link: \`${url}/posts/${post.id}\`\nrating: \`${post.rating}\`\nscore: \`${post.score.total}\`\n${post.file.url}`)
+			// for (const [key, value] of Object.entries(result['posts'])) {
+			// 	await interaction.reply(`link: \`${url}/posts/${value.id}\`\nrating: \`${value.rating}\`\nscore: \`${value.score.total}\`\n${value.file.url}`)
+			// }
 		}else{
 			await interaction.reply({ content: `Sorry no results found for your query \`${tags}\``})
 		}
