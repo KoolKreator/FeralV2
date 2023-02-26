f74a2064a9acf3639c3353bbd67f8846804483ac
Criptc <96071765+Criptc@users.noreply.github.com>
2023-02-25 21:16:06 -0600

    fixed unused var and bad define

--- src/commands/e621.js
+++ src/commands/e621.js
@@ -7,50 +7,43 @@ module.exports = {
 	.setName('e621')
 	.setDescription('Get posts from e621 or e926.')
 	.addStringOption(option => option.setName('tags').setDescription('Tags to search with').setRequired(true))
-    // .addNumberOption(option => option.setName('limit').setDescription('Amount of posts to return'))
-    .addBooleanOption(option => option.setName('adult').setDescription('Show adult rated post')),
-	// .addBooleanOption(option => option.setName('randomize').setDescription('randomize the search result')),
+    .addNumberOption(option => option.setName('limit').setDescription('Amount of posts to return'))
+    .addBooleanOption(option => option.setName('adult').setDescription('Show adult rated post'))
+	.addBooleanOption(option => option.setName('randomize').setDescription('randomize the search result')),
 	async execute(interaction) {
 		const tags = interaction.options.getString('tags')
-		// let limit = interaction.options.getNumber('limit')
+		let limit = interaction.options.getNumber('limit')
 		const nsfw = interaction.options.getBoolean('adult')
-
-		function getRandomResponse(max){
-			return Math.floor(Math.random() * (max - 0) + 0);
-		}
+		let site_url, rate;
 
 		if (nsfw){
-			eurl = "https://e621.net/posts"
+			site_url = "https://e621.net/posts"
 			rate = 'e'
 		} else {
-			eurl = "https://e926.net/posts"
+			site_url = "https://e926.net/posts"
 			rate = 's'
 		}
 
-		// if (limit === null){
-		// 	limit = 5
-		// } else if (limit > limit_max){
-		// 	limit = limit_max
-		// }
+		if (limit === null){
+			limit = 5
+		} else if (limit > limit_max){
+			limit = limit_max
+		}
 
-        const url = `${eurl}.json?tags=${encodeURIComponent(tags)}&rating=${rate}`;
-
+        const url = `${site_url}.json?tags=${encodeURIComponent(tags)}&rating=${rate}&limit=${limit}`;
 		let response = await fetch(url, {
-					method: "GET",
-					headers: {
-					"User-Agent": "FeralBot/2.0 (by Kr8tiveKanine on e621)"
-					}
-				})
+		    method: "GET",
+		    headers: {
+			"User-Agent": "FeralBot/1.0 (by Kr8tiveKanine on e621)"
+		    }
+		})
 
 		let result = await response.json();
-		let postnum = getRandomResponse(result.posts.length)
 
-		if (result.posts.length > -1){
-			let post = result.posts[postnum]
-			await interaction.reply(`link: \`${url}/posts/${post.id}\`\nrating: \`${post.rating}\`\nscore: \`${post.score.total}\`\n${post.file.url}`)
-			// for (const [key, value] of Object.entries(result['posts'])) {
-			// 	await interaction.reply(`link: \`${url}/posts/${value.id}\`\nrating: \`${value.rating}\`\nscore: \`${value.score.total}\`\n${value.file.url}`)
-			// }
+		if (result['posts'].length > 0){
+			for (const [_, value] of Object.entries(result['posts'])) {
+				await interaction.reply(`Link: \`${site_url}/${value.id}\`\nRating: \`${value.rating}\`\nScore: \`${value.score.total}\`\n${value.file.url}`)
+			}
 		}else{
 			await interaction.reply({ content: `Sorry no results found for your query \`${tags}\``})
 		}
